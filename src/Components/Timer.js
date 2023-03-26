c77875ef5a444e5f894a2443ee55206df5ea1ff4
Jessie <hire.jesslw@gmail.com>
2023-03-26 09:06:34 -1000

    leaderboard fetches from db, still needs css

--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -1,8 +1,16 @@
-import { getUserName, signOutUser, firestore,signIn, saveScore } from "../Firebase";
-import { addDoc, collection } from "firebase/firestore";
+import { useNavigate } from "react-router-dom";
+import { saveScore } from "../Firebase";
+
+
 export const Timer = ({time, reset}) => {
     const minutes = Math.floor(time / 60);
     const seconds = time % 60;
+
+    const navigate= useNavigate()
+    const saveAndGo = (time) => {
+        saveScore(time);
+        navigate("/leaderboard")
+    }
 
 
 
@@ -10,7 +18,8 @@ export const Timer = ({time, reset}) => {
         <div className="Timer">
           <p>Time elapsed: {minutes}:{seconds < 10 ? "0" : ""}{seconds}</p>
             <button type="button" onClick={(e) => reset()}>Play again</button>
-            <button type="button" onClick= {(e) => saveScore(time)}>Save Score</button>
+            <button type="button" onClick= {(e) => saveAndGo(time)}>Save Score</button>
+            <button type="button" onClick={(e) =>navigate("/")}>Home</button>
         </div>
       );
 }
--- src/Pages/Leaderboard.js
+++ src/Pages/Leaderboard.js
@@ -1,9 +1,64 @@
+import { Spinner } from "react-bootstrap";
+import { doc, collection, query, orderBy, getDocs } from "firebase/firestore";
+import { firestore } from "../Firebase";
+import { useEffect, useState } from "react";
+
 
 const Leaderboard = () => {
+    const [downloading, setDownloading] = useState(false)
+    const [board, setBoard] = useState([])
+
+    
+
+    const formatTime = (time) => {
+        const minutes = Math.floor(time / 60);
+        const seconds = time % 60;
+        return (
+            <div className ="time">
+                {minutes}:{seconds < 10 ? "0" : ""}{seconds}
+            </div>
+        )
+    }
+
+
+
+    const getNames =  async () => {
+        try {
+            setDownloading(true);
+            const q = query(collection(firestore, 'leaderboard'), orderBy('time', 'asc'));
+            const qSnap = await getDocs(q);
+                qSnap.forEach((doc) => {
+                    console.log(doc.id, "=>", doc.data())
+                    setBoard((prev) => [...prev, doc.data()])
+                });
+              } catch (error) {
+                console.log(error);
+              }  finally {
+                setDownloading(false)
+                console.log(board)
+              }
+        }
+
+        useEffect(() =>{
+            getNames()
+        }, [])
 
     return (
-        <div>
-            Hello from Leaderboard
+        <div className="board">
+            { downloading ? 
+                <div>
+                    <Spinner animation="border" variant="light">
+                        <span className="visually-hidden">Loading...</span>
+                    </Spinner>
+                </div>
+                :
+                <div className="user">
+                   { 
+                        board.map((i) => <div key = {i.index} className="name"> {i.name} {formatTime(i.time)} </div>)
+                   }
+                </div>
+
+            }
         </div>
     )
 }
